8f4c2325ecbfc1f192c4ec6cd8cea30edd3e3ec2
AnuradhaDev96 <anusampath9470@gmail.com>
2023-09-08 16:18:49 +0530

    Resolve pub warnings

--- test/quick_input_formatters_test.dart
+++ test/quick_input_formatters_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quick_input_formatters/quick_input_formatters.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
